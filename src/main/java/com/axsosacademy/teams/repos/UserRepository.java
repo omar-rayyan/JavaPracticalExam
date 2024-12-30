ab284d9d1266edb959d5f5d5fc915c9de733c06d
omar-rayyan <omarrayyan999@gmail.com>
2024-12-30 10:22:35 +0200

    initial commit

---
+++ src/main/java/com/axsosacademy/teams/repos/UserRepository.java
@@ -0,0 +1,12 @@
+package com.axsosacademy.teams.repos;
+
+import com.axsosacademy.teams.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
